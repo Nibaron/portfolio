a00e732f5f03ded571b1c0dbcef2208d90596a0e
Nibaron Kumar Das <36547410+Nibaron@users.noreply.github.com>
2023-08-29 18:33:44 +0600

    desktop navbar added

---
+++ src/Navbar.js
@@ -0,0 +1,18 @@
+
+const Navbar = () => {
+    return ( 
+        <nav id="dekstop-nav">
+        <div className="logo">Nibaron Kumar Das</div>
+        <div>
+            <ul className="nav-links">
+                <li><a href="#about"> About </a></li>
+                <li><a href="#experience"> Experience </a></li>
+                <li><a href="#projects"> Projects </a></li>
+                <li><a href="#contact"> Contact </a></li>
+            </ul>
+        </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
