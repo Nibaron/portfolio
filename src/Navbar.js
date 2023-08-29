a3ab83f4040363f98856ca04f3e5fc07637ae621
Nibaron Kumar Das <36547410+Nibaron@users.noreply.github.com>
2023-08-29 19:08:59 +0600

    navbar added

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,23 @@
 
 const Navbar = () => {
-    return ( 
-        <nav id="dekstop-nav">
+
+    function toggleMenu() {
+        const desktopMenu = document.querySelector("#desktop-nav");
+        const hamburgerMenu = document.querySelector("#hamburger-nav");
+    
+        const menuLinks = document.querySelector(".menu-links");
+        const icon = document.querySelector(".hamburger-icon");
+    
+        desktopMenu.classList.toggle("open");
+        hamburgerMenu.classList.toggle("open");
+        menuLinks.classList.toggle("open");
+        icon.classList.toggle("open");
+    }
+    
+
+    return (
+    <>
+        <nav id="desktop-nav" className="desktop-nav">
         <div className="logo">Nibaron Kumar Das</div>
         <div>
             <ul className="nav-links">
@@ -12,6 +28,24 @@ const Navbar = () => {
             </ul>
         </div>
         </nav>
+
+        <nav id="hamburger-nav" className="hamburger-nav">
+        <div className="logo">Nibaron Kumar Das</div>
+        <div className="hamburger-menu">
+        <div className="hamburger-icon" onClick={toggleMenu}>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        <div className="menu-links">
+            <li><a href="#about" onClick={toggleMenu}> About </a></li>
+            <li><a href="#experience" onClick={toggleMenu}> Experience </a></li>
+            <li><a href="#projects" onClick={toggleMenu}> Projects </a></li>
+            <li><a href="#contact" onClick={toggleMenu}> Contact </a></li>
+        </div>
+        </div>
+        </nav>
+    </>
      );
 }
  
